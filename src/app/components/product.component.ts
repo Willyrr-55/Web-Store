af957e5a1b18a10242f88044fe2dc78fbecfc2c4
Willian Adonay Rosa Ramirez <willyrosaramirezc1@gmail.com>
2021-10-07 23:55:15 -0600

    Input y Ouput

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,17 +1,17 @@
-import {Component} from '@angular/core';
-import {Product} from "../product.model"
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Product} from "../product.model";
  
 @Component({
     selector: "app-product",
-    templateUrl: "./product.component.html",
+    templateUrl: "./product.component.html"
 })
     
-    export class ProductComponent {
-        product: Product = {
-            id: '1',
-            image: 'assets/images/camiseta.png',
-            title: 'Camiseta',
-            price: 80000,
-            description: 'bla bla bla bla bla'
-        }
-}
+export class ProductComponent {
+
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter <any> = new EventEmitter();
+    addCart(){
+        console.log("añadir al carrito");
+        this.productClicked.emit(this.product.id)
+    }
+}   
